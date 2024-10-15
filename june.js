c9542c2d5c61c5fa7ff7e189a82aed80080d7466
Quadrimus <dev@quadrimus.com>
2024-10-15 22:20:09 +0200

    Fixed june.js

--- june.js
+++ june.js
@@ -1,23 +1,9 @@
-function colorizeLetters(element, colors) {
-	const text = element.textContent;
-	element.textContent = null;
-	for (let i = 0; i < text.length; i++) {
-		const span = document.createElement('span');
-		span.style.color = colors[i];
-		span.textContent = text.charAt(i);
-		element.appendChild(span);
-	}
-}
-
 function isJune() {
 	return new Date().getMonth() === 5;
 }
 
 function june() {
 	if (isJune() || document.location.hash === '#june') {
-		const dark = document.querySelector('h1 > .dark');
-		const pink = document.querySelector('h1 > .pink');
-		colorizeLetters(dark, ['#a00000', '#e00000', '#ff8000', '#e0e000']);
-		colorizeLetters(pink, ['#008000', '#0000e0', '#e000e0', '#a92471']);
+		document.querySelector('h1 .quadrimus').style.background = 'linear-gradient(90deg in hsl longer hue, #f00000 0%, #a92471 100%) text';
 	}
 }
