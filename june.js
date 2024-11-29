0c7ae69a11226e47d3ed7106ab4df05bd1cb42bd
Quadrimus <dev@quadrimus.com>
2024-11-29 14:05:42 +0100

    Reworked icons and removed june script

--- june.js
+++
@@ -1,9 +0,0 @@
-function isJune() {
-	return new Date().getMonth() === 5;
-}
-
-function june() {
-	if (isJune() || document.location.hash === '#june') {
-		document.querySelector('h1 .quadrimus').style.background = 'linear-gradient(90deg in hsl longer hue, #f00000 0%, #a92471 100%) text';
-	}
-}
